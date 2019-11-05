8813434e6657e7e89719f71a89d909b8ea182105
hrc2021 <hrc2021@users.noreply.github.com>
2019-11-05 13:53:10 -0500

    WebApp_Dev
    the begining

---
+++ LID_WebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LID_WebApp.Pages {
+    public class PrivacyModel : PageModel {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            string dateTime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
