ab0850792186405dd0f414d490b0c20d52c58666
ccrgoalie9 <32224360+ccrgoalie9@users.noreply.github.com>
2019-11-11 16:53:21 -0500

    Update Download.cs
    Comments

--- LID_Framework/LID_ClassLibrary/Download.cs
+++ LID_Framework/LID_ClassLibrary/Download.cs
@@ -2,17 +2,25 @@
 using System.Net;
 
 namespace LID_ClassLibrary {
+
+    //This class downloads and parses the Bullentin from the IIP website. 
     public class Download {
+        //Global Variable for outfile name
         string outfile;
 
+        //Constructor
         public Download(Config config) {
             DownloadFromWeb(config);
         }
 
+        //Loads the bullentin in and parses out the Data.
         public void DownloadFromWeb(Config config) {
+            //Outfile Naming so that verision can be kept and a path from config file is present.
             outfile = (config.DirPath + @"\Bulletins\" + DateTime.UtcNow.ToString("yyyy-MM-dd") + "_Bulletin_Pull.txt");
+            //Calls Download to grab the html from the bullentin website
             DownloadFile(config.BulletinUrl, outfile);
-
+            
+            //Parser and neatness
             string text = System.IO.File.ReadAllText(outfile);
             text = text.Replace("</p>", "");
             text = text.Replace("<p>", "");
@@ -23,6 +31,7 @@ namespace LID_ClassLibrary {
             System.IO.File.WriteAllText(outfile, text);
         }
 
+        //Method that downloads the Bullentin using System.net
         public static void DownloadFile(string Bulletin, string SavedBulletin) {
             WebClient client = new WebClient();
             client.DownloadFile(Bulletin, SavedBulletin);
