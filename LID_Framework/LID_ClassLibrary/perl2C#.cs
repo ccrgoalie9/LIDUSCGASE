f557e118b03f61d080b5e2805ef5bef7417f69bb
CarbonCGAutonomous <CarbonCGAutonomous@users.noreply.github.com>
2019-11-11 22:10:46 -0500

    Added Luke's Class to project
    Just added Luke's class to the name space so that it is loaded and can be viewed at startup in VS

--- LID_Framework/LID_ClassLibrary/perl2C#.cs
+++ LID_Framework/LID_ClassLibrary/perl2C#.cs
@@ -1,58 +1,54 @@
 ﻿using System;
 
-public class perl2Csharp
-{
-	public perl2Csharp()
-	{
-        // Case statement -- encode message based upon type
-        // Common code. All messages start with the same 38 bits....
+namespace LID_ClassLibrary {
+    public class perl2Csharp {
+        public perl2Csharp() {
+            // Case statement -- encode message based upon type
+            // Common code. All messages start with the same 38 bits....
 
-        ////////////////////////////////////////////////////////////
-        // Bits Len   Description
-        //    0-5       6   Message Type
-        //    6-7       2   Repeat Indicator
-        //    8-37     30   MMSI
+            ////////////////////////////////////////////////////////////
+            // Bits Len   Description
+            //    0-5       6   Message Type
+            //    6-7       2   Repeat Indicator
+            //    8-37     30   MMSI
 
-        payload = sprintf("%06b", int type). "00".sprintf("%030b", int($mmsi))
+            //payload = sprintf("%06b", int type). "00".sprintf("%030b", int($mmsi))
+    
 
 
+        try {
+                Console.WriteLine("binary message test");
+                string authorName = "";
+                int type = 8;
+                string bookTitle = "ADO.NET Programming using C#";
+                bool mvp = true;
+                double price = 54.99;
 
-        try
-        {
-            Console.WriteLine("binary message test");
-            string authorName = "";
-            int type = 8;
-            string bookTitle = "ADO.NET Programming using C#";
-            bool mvp = true;
-            double price = 54.99;
+                string fileName = @"C:\temp\MC.bin";
+                BinaryWriter bwStream = new BinaryWriter(new FileStream(fileName, FileMode.Create));
 
-            string fileName = @"C:\temp\MC.bin";
-            BinaryWriter bwStream = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+                Encoding ascii = Encoding.ASCII;
+                BinaryWriter bwEncoder = new BinaryWriter(new FileStream(fileName, FileMode.Create), ascii);
 
-            Encoding ascii = Encoding.ASCII;
-            BinaryWriter bwEncoder = new BinaryWriter(new FileStream(fileName, FileMode.Create), ascii);
-
-            using (BinaryWriter binWriter =
-                new BinaryWriter(File.Open(fileName, FileMode.Create)))
-            {
-                // Write string   
-                binWriter.Write(authorName);
-                // Write string   
-                // Write integer  
-                binWriter.Write(age);
-                binWriter.Write(bookTitle);
-                // Write boolean  
-                binWriter.Write(mvp);
-                // Write double   
-                binWriter.Write(price);
+                using(BinaryWriter binWriter =
+                    new BinaryWriter(File.Open(fileName, FileMode.Create))) {
+                    // Write string   
+                    binWriter.Write(authorName);
+                    // Write string   
+                    // Write integer  
+                    binWriter.Write(age);
+                    binWriter.Write(bookTitle);
+                    // Write boolean  
+                    binWriter.Write(mvp);
+                    // Write double   
+                    binWriter.Write(price);
+                }
+                Console.WriteLine("Data Written!");
+                Console.WriteLine();
+            } catch(IOException ioexp) {
+                Console.WriteLine("Error: {0}", ioexp.Message);
             }
-            Console.WriteLine("Data Written!");
-            Console.WriteLine();
-        }
-        catch (IOException ioexp)
-        {
-            Console.WriteLine("Error: {0}", ioexp.Message);
-        }
 
+        }
     }
 }
