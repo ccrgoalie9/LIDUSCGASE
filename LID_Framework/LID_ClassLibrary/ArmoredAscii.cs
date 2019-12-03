fd69c0c56560d07a7671432fd99cb0fd097bacad
CarbonCGAutonomous <46693740+CarbonCGAutonomous@users.noreply.github.com>
2019-12-03 14:53:56 -0500

    Added ArmoredAscii to project

--- LID_Framework/LID_ClassLibrary/ArmoredAscii.cs
+++ LID_Framework/LID_ClassLibrary/ArmoredAscii.cs
@@ -4,27 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LID_ClassLibrary
-{
-    class ArmoredAscii
-    { 
-        public ArmoredAscii(List<string> input, Config config)
-        {
+namespace LID_ClassLibrary {
+    class ArmoredAscii {
+
+        List<string> AsciiStream;
+        public ArmoredAscii(List<string> input, Config config) {
+            AsciiStream = new List<string>();
             ConvertToAscii(input);
         }
 
-        public void ConvertToAscii(List<string> input)
-        {
+        public void ConvertToAscii(List<string> input) {
             List<string> BinaryStream = input;
-
-            /*foreach(String x in BinaryStream)
-            {
-                List<string> AsciiStream = Encoding.ASCII.GetString(byte[], BinaryStream);
-            }*/
-
+            string temp = "";
+            byte[] convert;
+            int count = 0;
+            foreach(String x in BinaryStream) {
+                convert = new byte[x.Length/6];
+                for(int i = 0; i < x.Length; i++) {
+                    temp += x[i];
+                    if(i % 6 == 0) {
+                        convert[0] = Convert.ToByte(temp);
+                        count++;
+                        temp = "";
+                    }
+                }
+                //AsciiStream.Add(Encoding.ASCII.GetString(convert[], 6));
+            }
 
         }
 
     }
-    
+
 }
