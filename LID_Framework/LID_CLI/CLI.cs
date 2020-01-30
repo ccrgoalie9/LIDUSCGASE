48f7151c74642eb68ea804292b28496f5156dc4a
CarbonCGAutonomous <CarbonCGAutonomous@users.noreply.github.com>
2020-01-30 13:55:35 -0500

    download Shifted

--- LID_Framework/LID_CLI/CLI.cs
+++ LID_Framework/LID_CLI/CLI.cs
@@ -9,98 +9,99 @@ using LID_ClassLibrary;
 namespace LID_CLI {
     class CLI {
         //Framework is the workhorse of the program
-        Framework framework;
+        private static Framework framework;
 
         static void Main(string[] args) {
             //See if Directories exist yet
             //If not make them
             DirectoryCheck();
-
+            framework = new Framework();
+            
             //Starts Error Checking file
             if(!File.Exists(framework.ErrorFile)) {
                 framework.ErrorFile = @"C:\Users\" + Environment.UserName + @"\Documents\LID Files\ErrorLogs\Error.txt";
                 File.Create(framework.ErrorFile).Dispose();
             }
-            string text = File.ReadAllText(Framework.ErrorFile);
+            string text = File.ReadAllText(framework.ErrorFile);
             if(!text.Contains(DateTime.UtcNow.ToString("yyyy-MM-dd"))) {
-                File.AppendAllText(Framework.ErrorFile, "Error Checking Starting: " + DateTime.UtcNow.ToString("yyyy-MM-dd") + "\n");
+                File.AppendAllText(framework.ErrorFile, "Error Checking Starting: " + DateTime.UtcNow.ToString("yyyy-MM-dd") + "\n");
             }
 
 
             try {//Get the current Bulletin
                 Console.Write("Fetching Current Bulletin...\t");
-                todayDownload = new Download(Framework);
+                todayDownload = new Download(framework);
                 Console.WriteLine("Current Bulletin Fetched");
             } catch(Exception x) {
                 Console.WriteLine("Error: Failed To Fetch The Bulletin\n" + x.Message);
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n"); //Write to error file
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n"); //Write to error file
             }
 
             try {//Get the necessary bits from the bulletin
-                todayScraper = new Scraper(todayDownload.GetOutFile(), Framework);
+                todayScraper = new Scraper(todayDownload.GetOutFile(), framework);
             } catch(Exception x) {
                 Console.WriteLine("Error: Failed To Scrape The Bulletin\n" + x.Message);
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
             }
 
             try {//Create the KML file in format: ICEBERGS_'date'.kml
                 Console.Write("Creating KML File...\t\t");
-                todayLine = new Line(todayScraper.GetCoordinatesIngestors(), Framework);
+                todayLine = new Line(todayScraper.GetCoordinatesIngestors(), framework);
                 Console.WriteLine("KML File Created");
             } catch(Exception x) {
                 Console.WriteLine("Error: Failed To Create The KML File\n" + x.Message);
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
             }
 
             try {//Math for the Bearings and Ranges
                 Console.Write("Creating Bearings And Ranges...\t");
-                todayBR = new BearingRange(todayScraper.GetCoordinatesIngestors(), Framework);
+                todayBR = new BearingRange(todayScraper.GetCoordinatesIngestors(), framework);
                 Console.WriteLine("Bearing And Ranges Created");
-                if(Framework.Debug) {
+                if(framework.Debug) {
                     todayBR.Debug();
                 }
             } catch(Exception x) {
                 Console.WriteLine("Error: Failed To Create The File");
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
             }
 
             try {//Create the Binary from the Bearing and Ranges
                 Console.Write("Creating Binary...\t\t");
-                todayBin = new BinaryCreator(todayBR.GetCoordinates(), Framework);
+                todayBin = new BinaryCreator(todayBR.GetCoordinates(), framework);
                 Console.WriteLine("Binaries Created");
-                if(Framework.Debug) {
+                if(framework.Debug) {
                     todayBin.Debug();
                 }
             } catch(Exception x) {
                 Console.WriteLine("Error: Failed To Create The Binary");
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
             }
 
             try {//Convert the Binary to Armored ASCII
                 Console.Write("Creating Armored ASCII...\t");
-                todayAscii = new ArmoredAscii(todayBin.LineMessages, Framework);
+                todayAscii = new ArmoredAscii(todayBin.LineMessages, framework);
                 Console.WriteLine("ASCII Created");
-                if(Framework.Debug) {
+                if(framework.Debug) {
                     todayAscii.Debug();
                 }
             } catch(Exception x) {
                 Console.WriteLine("Error: Failed To Create Armored ASCII");
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
 
             }
         }
         private static void DirectoryCheck() {
             Console.Write("Updating Directories...\t\t");
-            try { Directory.CreateDirectory(Framework.DirPath + @"\Bulletins"); } catch(Exception x) {
-                Framework.CreateConfig();
-                Framework.ReadConfig();
+            try { Directory.CreateDirectory(framework.DirPath + @"\Bulletins"); } catch(Exception x) {
+                framework.CreateConfig();
+                framework.ReadConfig();
                 Console.WriteLine("Error Creating Directories, Attempting To Fix By Re-Writing Configuration File");
-                File.AppendAllText(Framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                File.AppendAllText(framework.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
             }
-            Directory.CreateDirectory(Framework.DirPath + @"\KML");
-            Directory.CreateDirectory(Framework.DirPath + @"\LatLongs");
-            Directory.CreateDirectory(Framework.DirPath + @"\Polar");
-            Directory.CreateDirectory(Framework.DirPath + @"\ErrorLogs");
+            Directory.CreateDirectory(framework.DirPath + @"\KML");
+            Directory.CreateDirectory(framework.DirPath + @"\LatLongs");
+            Directory.CreateDirectory(framework.DirPath + @"\Polar");
+            Directory.CreateDirectory(framework.DirPath + @"\ErrorLogs");
             Console.WriteLine("Directories Updated");
         }
 
--- LID_Framework/LID_ClassLibrary/Framework.cs
+++ LID_Framework/LID_ClassLibrary/Framework.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,7 +30,122 @@ namespace LID_ClassLibrary {
 
         //
         public Framework() {
-
+            ConfigPath = @"config.txt";
+            if(!File.Exists(ConfigPath)) {
+                //Create file from defaults
+                CreateConfig();
+            }
+            if(File.Exists(ConfigPath)) {
+                ReadConfig();
+            }
         }
+
+        //Modifiers
+        public void ReadConfig() {
+            int flag = 0;
+            int flagCount = 11;
+            try {
+                using(StreamReader configReader = new StreamReader(ConfigPath)) {
+                    string temp;
+                    while((temp = configReader.ReadLine()) != null) {
+                        if(temp.StartsWith("#")) { continue; }
+                        if(temp.Contains("Files Directory Location")) { DirPath = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("Bulletin URL")) { BulletinUrl = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("Chart URL")) { ChartUrl = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("Website URL")) { WebUrl = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("KML Color Berg Limit")) { KmlColor1 = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("KML Color Est Berg Limit")) { KmlColor2 = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("KML Color Sea Ice Limit")) { KmlColor3 = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        if(temp.Contains("Error File Location")) { ErrorFile = temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1); flag++; }
+                        try {
+                            if(temp.Contains("KML Line Width")) { KmlWidth = Convert.ToInt32(temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1)); flag++; }
+                        } catch(Exception x) {
+                            Console.WriteLine("Invalid Value for 'KML Line Width'\n" + x.Message);
+                            File.AppendAllText(ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                            //If error, default is 5
+                            KmlWidth = 5;
+                        }
+                        try {
+                            if(temp.Contains("Message MMSI")) { MMSI = Convert.ToInt32(temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1)); flag++; }
+                        } catch(Exception x) {
+                            Console.WriteLine("Invalid Value for 'MMSI'\n" + x.Message);
+                            File.AppendAllText(ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                            //If error, default is 003679999
+                            MMSI = 003679999;
+                        }
+                        try {
+                            if(temp.Contains("Debug")) { Debug = Convert.ToBoolean(temp.Substring(temp.IndexOf('\'') + 1, temp.LastIndexOf('\'') - temp.IndexOf('\'') - 1)); flag++; }
+                        } catch(Exception x) {
+                            Console.WriteLine("Invalid Value For An Expected Boolean\n" + x.Message);
+                            File.AppendAllText(ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                        }
+                    }
+                    if(flag < flagCount) {
+                        throw new Exception("Not All Expected Values Were Present");
+                    }
+                }
+            } catch(Exception x) {
+                Console.WriteLine("Error in reading configuration file, re-writing");
+                ErrorFile = @"C:\Users\" + Environment.UserName + @"\Documents\LID Files\ErrorLogs\Error.txt";
+                CreateConfig();
+                ReadConfig();
+                File.AppendAllText(ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+            }
+        }
+
+        //Create the configuration file for first time or for new users
+        public void CreateConfig() {
+            using(StreamWriter configWriter = new StreamWriter(ConfigPath, false)) {
+                configWriter.WriteLine("#Configuration file for the LID program, please only edit between the single quotes");
+                configWriter.WriteLine("#Config path: " + Environment.CurrentDirectory + "\n");
+                configWriter.WriteLine(@"Files Directory Location: 'C:\Users\" + Environment.UserName + @"\Documents\LID Files'");
+                configWriter.WriteLine(@"Error File Location: 'C:\Users\" + Environment.UserName + @"\Documents\LID Files\ErrorLogs\Error.txt'");
+                configWriter.WriteLine("\nUpdate links only if they have changed");
+                configWriter.WriteLine("Website URL: 'https://lidtesting.azurewebsites.net'");
+                configWriter.WriteLine(@"Bulletin URL: 'https://www.navcen.uscg.gov/?pageName=iipB12Out'");
+                configWriter.WriteLine(@"Chart URL: 'https://www.navcen.uscg.gov/?pageName=iipCharts&Current'");
+                configWriter.WriteLine("\n#KML file parameters");
+                configWriter.WriteLine(@"#Color is set by TTBBGGRR");
+                configWriter.WriteLine(@"#TT is the transparency from 00 being clear to FF being opaque.");
+                configWriter.WriteLine(@"#BB, GG, and RR are the level of blue, green, and red respectively");
+                configWriter.WriteLine(@"KML Color Berg Limit    : 'ffc702ff'");
+                configWriter.WriteLine(@"KML Color Est Berg Limit: 'ff00ffff'");
+                configWriter.WriteLine(@"KML Color Sea Ice Limit : 'ffffff00'");
+                configWriter.WriteLine(@"KML Line Width: '5'");
+                configWriter.WriteLine(@"Message MMSI: '003679999'");
+                configWriter.WriteLine(@"Debug: 'False'");
+            }
+        }
+
+        public void DownloadFromWeb(Config config) {
+            //Outfile Naming so that verision can be kept and a path from config file is present.
+            outfile = (config.DirPath + @"\Bulletins\" + DateTime.UtcNow.ToString("yyyy-MM-dd") + "_Bulletin_Pull.txt");
+            //Calls Download to grab the html from the bullentin website
+            DownloadFile(config.BulletinUrl, outfile);
+
+            //Parser and neatness
+            string text = System.IO.File.ReadAllText(outfile);
+            text = text.Replace("</p>", "");
+            text = text.Replace("<p>", "");
+            text = text.Replace("	", "");
+            text = text.Replace(".", ". ");
+            text = text.Replace(",", ", ");
+            text = text.Substring(text.IndexOf("NORTH AMERICAN ICE SERVICE (NAIS)"), text.IndexOf("CANCEL THIS MSG") - text.IndexOf("NORTH AMERICAN ICE SERVICE (NAIS)") + 31);
+            System.IO.File.WriteAllText(outfile, text);
+        }
+
+        //Method that downloads the Bullentin using System.net
+        public static void DownloadFile(string Bulletin, string SavedBulletin) {
+            WebClient client = new WebClient();
+            client.DownloadFile(Bulletin, SavedBulletin);
+            client.Dispose();
+        }
+
+        //Accessors
+        public string GetOutFile() {
+            return (outfile);
+        }
+
+        //End of CLI
     }
 }
