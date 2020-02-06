d904d72b49a29940e6bab30b2c36cf0c570280cb
CarbonCGAutonomous <CarbonCGAutonomous@users.noreply.github.com>
2020-02-06 09:10:14 -0500

    Intermediate update
    Remaining tasks, BR class, Armored ASCII class, Binary Creator class

--- LID_Framework/LID_ClassLibrary/Framework.cs
+++ LID_Framework/LID_ClassLibrary/Framework.cs
@@ -22,13 +22,26 @@ namespace LID_ClassLibrary {
         public int KmlWidth { get; set; }
         public string ErrorFile { get; set; }
         public bool Debug { get; set; }
+        //Scraper variables
+        private string inFileName, degOutFile, decOutFile, output, decOutput, ingested;
+        private string[] lineTypes, ingests, coordsIngested;
+        readonly private string partialPath;
+        readonly private string[] append = { "A", "B", "C", "D", "E", "F", "G" };
+        //Ingestor Variables
+        //private string ingested;
+        //private string output;
+        private string lineType;
+        private double[,] coords;
+        //Line Varaibles
+        readonly string filepath;
+        //Polar Variables
+        double a, c, d, R, phi1, phi2, deltaphi, deltalambda, theta1, theta, theta2;
+        readonly double[][,] PolarSets;
+        //string output;
+        //readonly string outFile;
         //
 
-        //
-
-        //
-
-        //
+        //Constructor
         public Framework() {
             ConfigPath = @"config.txt";
             if(!File.Exists(ConfigPath)) {
@@ -39,6 +52,7 @@ namespace LID_ClassLibrary {
                 ReadConfig();
             }
         }
+
 
         //Modifiers
         public void ReadConfig() {
@@ -92,6 +106,393 @@ namespace LID_ClassLibrary {
                 File.AppendAllText(ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
             }
         }
+        //Scraper Methods
+        //Read from the file
+        private void ReadFile() {
+            try {
+                ingested = "";
+                using(StreamReader coordFile = new StreamReader(inFileName)) {
+                    ingested = coordFile.ReadToEnd();
+                }
+            } catch(Exception x) {
+                Console.WriteLine(x.Message);
+                File.AppendAllText(config.ErrorFile, DateTime.UtcNow.ToString("HH:mm:ss") + " : " + x.Message + "\n");
+                ingested = "Read Failed";
+            }
+        }
+
+        //Create the coordinatesIngested array
+        private void ScrapeFile() {
+            ingests = ingested.Split(' ');
+            int count = 0;
+            //Determine necessary length of array
+            foreach(string x in ingests) {
+                try {
+                    if(x.Length >= 6 && x.Length <= 8 && !x.Contains("Z")) {
+                        if(x.EndsWith(".")) {
+                            Convert.ToInt32(x.Substring(x.Length - 3, 1));
+                            count++;
+                        }
+                    }
+                } catch(Exception) {
+                    //Do nothing
+                }
+            }
+            //Set arrays of specific length
+            coordsIngested = new string[count];
+            lineTypes = new string[count];
+            string temp = "";
+            count = 0;
+            //Temporary Variables for the 
+            string temp1 = "";
+            string temp2 = "";
+            string tempHeader = "";
+            //Parse for coordinates
+            foreach(string x in ingests) {
+                //For Section Headers
+                if(x == "ESTIMATED" || x == "WESTERN" || x == "EASTERN" || x == "NORTHERN" || x == "SOUTHERN" || x == "SEA") {
+                    temp1 = x;
+                }
+                if(x == "ICEBERG" || x == "ICE") {
+                    temp2 = x;
+                }
+                if(x == "LIMIT") {
+                    if(temp1 != "") tempHeader = (temp1 + " " + temp2 + " " + x);
+                    else if(temp2 != "") tempHeader = (temp2 + " " + x);
+                    temp1 = "";
+                    temp2 = "";
+                }
+
+                //For Coordinates
+                try {
+                    if(x.Length >= 6 && x.Length <= 8 && !x.Contains("Z")) {
+                        if(x.EndsWith(".")) {
+                            Convert.ToInt32(x.Substring(x.Length - 3, 1));
+                            temp += x;
+                            if(tempHeader != "") lineTypes[count] = tempHeader;
+                            else lineTypes[count] = lineTypes[count - 1];
+                            tempHeader = "";
+                            coordsIngested[count] = temp;
+                            count++;
+                            temp = "";
+                        } else if(x.EndsWith(",")) {
+                            Convert.ToInt32(x.Substring(x.Length - 3, 1));
+                            temp += x;
+                        } else {
+                            Convert.ToInt32(x.Substring(x.Length - 2, 1));
+                            temp += x + " ";
+                        }
+                    }
+                } catch(Exception) {
+                    //Do nothing
+                }
+            }
+        }
+
+        private void CreateOutput() {
+            //Create Degree Output
+            try {
+                int count = 0;
+                output = "";
+                foreach(string x in coordsIngested) {
+                    output += lineTypes[count];
+                    if(count == 0) {
+                        output += DateTime.UtcNow.ToString(" yyyy-MM-dd");
+                    }
+                    output += "\n";
+                    output += x + "\n";
+                    count++;
+                }
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+                output = e.Message;
+                Console.ReadKey(); //Error
+            }
+
+            //Create Decimal Output
+            try {
+                int count = 0;
+                decOutput = "";
+                foreach(Ingestor x in coordinates) {
+                    decOutput += lineTypes[count];
+                    if(count == 0) {
+                        decOutput += DateTime.UtcNow.ToString(" yyyy-MM-dd");
+                    }
+                    decOutput += "\n";
+                    decOutput += x.GetOutput() + "\n";
+                    count++;
+                }
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+                decOutput = e.Message;
+                Console.ReadKey();
+            }
+        }
+
+        //Convert coordinates using the Ingestor class
+        private void ConvertIngestor(int check) {
+            coordinates = new Ingestor[coordsIngested.Length];
+
+            //Add Letters To Reflect Subsections of the same Set
+            string indexes = "";
+            string temp = "";
+            for(int i = 0; i < lineTypes.Length - 1; i++) {
+                temp = "";
+                if(lineTypes[i] == lineTypes[i + 1]) {
+                    temp = (i + 1).ToString();
+                    indexes += i.ToString() + " ";
+                }
+            }
+            if(temp != "") {
+                indexes += temp;
+            }
+            if(indexes.Split().Length > 1) {
+                int[] indexer = new int[indexes.Split().Length];
+                int count = 0;
+                foreach(string x in indexes.Split()) {
+                    indexer[count] = Convert.ToInt32(x);
+                    count++;
+                }
+                for(int i = 0; i < indexer.Length; i++) {
+                    lineTypes[indexer[i]] += " " + append[i];
+                }
+            }
+            //End Adding Letters
+
+            if(check != 3) {
+                for(int i = 0; i < coordinates.Length; i++) {
+                    coordinates[i] = new Ingestor(coordsIngested[i], i + 1);
+                    coordinates[i].SetLineType(lineTypes[i]);
+                }
+            } else {
+                for(int i = 0; i < coordinates.Length; i++) {
+                    coordinates[i] = new Ingestor(coordsIngested[i], i + i, check);
+                    coordinates[i].SetLineType(lineTypes[i]);
+                }
+            }
+        }
+
+        //Ingestor Methods
+        //Convert ingest to array
+        private void ConvertInput() {
+            try {
+                ingested = ingested.Replace(", ", ","); //Get rid of pesky spaces
+                string[] tempByComma = ingested.Split(',');
+                int length = tempByComma.Length;
+                coords = new double[length, 2];
+                //Convert from lat/long to degrees
+                //North is Positive, East is Positive
+                int left = 0;
+                foreach(string line in tempByComma) {
+                    string[] tempBySpace = line.Split(' ');
+                    string temp1;
+                    string temp2;
+                    string direction;
+                    int sign;
+
+                    //Convert Lat
+                    temp1 = tempBySpace[0].Substring(0, 2);
+                    temp2 = tempBySpace[0].Substring(3, 2);
+                    direction = tempBySpace[0].Substring(5, 1);
+                    //Determine sign based on direction
+                    if(direction == "S") {
+                        sign = -1;
+                    } else {
+                        sign = 1;
+                    }
+                    coords[left, 0] = (Math.Truncate((Convert.ToDouble(temp1) + Convert.ToDouble(temp2) / 60) * sign * 1000000)) / 1000000;
+
+                    //Convert Long
+                    temp1 = tempBySpace[1].Substring(0, 3);
+                    temp2 = tempBySpace[1].Substring(4, 2);
+                    direction = tempBySpace[1].Substring(6, 1);
+                    //Determine the sign based on direction
+                    if(direction == "W") {
+                        sign = -1;
+                    } else {
+                        sign = 1;
+                    }
+                    coords[left, 1] = (Math.Truncate((Convert.ToDouble(temp1) + Convert.ToDouble(temp2) / 60) * sign * 1000000)) / 1000000;
+
+                    //Increment the array
+                    left++;
+                }
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+                //To easily determine if something went wrong
+                coords = new double[,] { { -1, -1 }, { -1, -1 } };
+            }
+        }
+
+        //Line methods
+
+        //Write the coordinate sets to a text file
+        private void WriteFile() {
+            //Output to Degree File
+            try {
+                using(StreamWriter decimalFile = new StreamWriter(degOutFile)) {
+                    decimalFile.Write(output);
+                }
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            //Output to Decimal File
+            try {
+                using(StreamWriter decimalFile = new StreamWriter(decOutFile)) {
+                    decimalFile.Write(decOutput);
+                }
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //Line Class
+        public Line(Ingestor[] input, Config config) {
+            filepath = (config.DirPath + @"\KML\" + DateTime.UtcNow.ToString("yyyy-MM-dd") + "_ICEBERGS.kml");
+            string filename = (DateTime.UtcNow.ToString("yyyy-MM-dd") + "_ICEBERGS");
+
+            //Check if file already exists
+            if(File.Exists(filepath)) {
+                File.Delete(filepath);
+            }
+
+            //Document Creation
+            var document = new Document {
+                Id = "KML",
+                Open = true,
+                Name = filename
+            };
+
+            //Styling
+            string colorCode1 = config.KmlColor1;
+            string colorCode2 = config.KmlColor2;
+            string colorCode3 = config.KmlColor3;
+            /*
+            LineStyle lineStyle = new LineStyle {
+                Color = Color32.Parse(colorCode),
+                Width = config.KmlWidth
+            };
+            */
+
+            /*
+            PolygonStyle PolyStyle = new PolygonStyle {
+                Color = Color32.Parse(colorCode)
+            };
+            */
+
+            //Timespan
+            SharpKml.Dom.TimeSpan lineTimespan = new SharpKml.Dom.TimeSpan {
+                Begin = Convert.ToDateTime(DateTime.UtcNow.ToString("yyyy-MM-dd") + " 00:00:01"),
+                End = Convert.ToDateTime(DateTime.UtcNow.ToString("yyyy-MM-dd") + " 23:59:59")
+            };
+
+
+            //Actual reads style and adds poly and line together
+            string styleIL = "lineIcebergLimit";
+            string styleEIL = "lineEstimatedIcebergLimit";
+            string styleSIL = "lineSeaIceLimit";
+            {
+                //Style for Iceberg Limit
+                Style SimpleStyle = new Style();
+                SimpleStyle.Id = styleIL;
+                SimpleStyle.Line = new LineStyle {
+                    Color = Color32.Parse(colorCode1),
+                    Width = config.KmlWidth
+                };
+                SimpleStyle.Polygon = new PolygonStyle {
+                    Color = Color32.Parse(colorCode1)
+                };
+                document.AddStyle(SimpleStyle);
+                //Style for Estimated Iceberg Limit
+                SimpleStyle = new Style();
+                SimpleStyle.Id = styleEIL;
+                SimpleStyle.Line = new LineStyle {
+                    Color = Color32.Parse(colorCode2),
+                    Width = config.KmlWidth
+                };
+                SimpleStyle.Polygon = new PolygonStyle {
+                    Color = Color32.Parse(colorCode2)
+                };
+                document.AddStyle(SimpleStyle);
+                //Style for Sea Ice Limit
+                SimpleStyle = new Style();
+                SimpleStyle.Id = styleSIL;
+                SimpleStyle.Line = new LineStyle {
+                    Color = Color32.Parse(colorCode3),
+                    Width = config.KmlWidth
+                };
+                SimpleStyle.Polygon = new PolygonStyle {
+                    Color = Color32.Parse(colorCode3)
+                };
+                document.AddStyle(SimpleStyle);
+            }
+
+            //LINE STRING & PLACEMARK CONSTRUCTION ZONE
+            foreach(Ingestor ingest in input) {
+                //One per segment
+                LineString linestring = new LineString();
+                CoordinateCollection coordinates = new CoordinateCollection();
+                linestring.AltitudeMode = AltitudeMode.ClampToGround;
+                linestring.Extrude = true;
+                linestring.Tessellate = true;
+
+                double[,] coordArray = ingest.GetCoordinates();
+
+                double[] lat = new double[(coordArray.Length / 2)];
+                double[] lon = new double[(coordArray.Length / 2)];
+
+                for(int i = 0; i < (coordArray.Length / 2); i++) {
+                    lon[i] = coordArray[i, 1];
+                }
+                for(int i = 0; i < (coordArray.Length / 2); i++) {
+                    lat[i] = coordArray[i, 0];
+                }
+
+                for(int i = 0; i < lon.Length; i++) {
+                    coordinates.Add(new Vector(lat[i], lon[i]));
+                }
+
+                linestring.Coordinates = coordinates;
+                Placemark placemark = new Placemark {
+                    Name = ingest.GetLineType(),
+                    Visibility = true,
+                    Geometry = linestring,
+                    //Moved StyleUrl out in order to be able to set it conditionally
+                    //Moved Description out as well
+                    Time = lineTimespan                                    //Timespan
+                };
+                if(ingest.GetLineType().Contains("ESTIMATED ICEBERG LIMIT")) {
+                    placemark.StyleUrl = new Uri(("#" + styleEIL), UriKind.Relative); //Uri makes url refrence to indocument style rather than cloud sourced
+                    placemark.Description = new Description() { Text = "Based off of data from Greenland" };
+                } else if(ingest.GetLineType().Contains("ICEBERG LIMIT")) {
+                    placemark.StyleUrl = new Uri(("#" + styleIL), UriKind.Relative);
+                    placemark.Description = new Description() { Text = "Based off of data from the IIP and Canadian Ice Patrol" };
+                } else if(ingest.GetLineType().Contains("SEA ICE LIMIT")) {
+                    placemark.StyleUrl = new Uri(("#" + styleSIL), UriKind.Relative);
+                    placemark.Description = new Description() { Text = "Sea ice within limit displayed" };
+                } else {
+                    placemark.StyleUrl = new Uri(("#" + styleIL), UriKind.Relative);
+                    placemark.Description = new Description() { Text = "Based off of data from the IIP and Canadian Ice Patrol" };
+                }
+
+                document.AddFeature(placemark);
+
+            }
+            //END LINE STRING CONSTRUCTION ZONE
+
+
+            //Creates KML assignes it from document
+            var kml = new Kml {
+                Feature = document
+            };
+
+            //Outputs KML File
+            KmlFile kmlFile = KmlFile.Create(kml, true);
+            using(FileStream stream = File.OpenWrite(filepath)) {
+                kmlFile.Save(stream);
+            }
+        }
 
         //Create the configuration file for first time or for new users
         public void CreateConfig() {
@@ -142,9 +543,6 @@ namespace LID_ClassLibrary {
         }
 
         //Accessors
-        public string GetOutFile() {
-            return (outfile);
-        }
 
         //End of CLI
     }
