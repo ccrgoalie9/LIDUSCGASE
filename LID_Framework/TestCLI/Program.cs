c75249913d7420796d692652e0974b10e2fbea4d
CarbonCGAutonomous <46693740+CarbonCGAutonomous@users.noreply.github.com>
2020-02-19 16:12:33 -0500

    IntermediateFixForTesting

---
+++ LID_Framework/TestCLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LID_ClassLibrary;
+
+namespace TestCLI {
+    class Program {
+        static void Main(string[] args) {
+            Config config = new Config();
+            DoIt today = new DoIt(config);
+            today.FullProcess();
+            System.Diagnostics.Process.Start("explorer.exe", config.DirPath);
+            Console.ReadKey();
+        }
+    }
+}
--- LID_Framework/TestCLI/Properties/AssemblyInfo.cs
+++ LID_Framework/TestCLI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("TestCLI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("TestCLI")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d716a832-6eb3-4335-9ec5-694a51d511f1")]
+[assembly: Guid("ffd18664-aa51-4483-a583-cbb06b1348d1")]
 
 // Version information for an assembly consists of the following four values:
 //
