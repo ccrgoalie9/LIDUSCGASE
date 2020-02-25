9c3d3e9f52696172612f6fc2d513a2f296ef7c99
CarbonCGAutonomous <46693740+CarbonCGAutonomous@users.noreply.github.com>
2020-02-25 17:19:36 -0500

    Fixed LID_CLI

---
+++ LID_Framework/LID_CLI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using LID_ClassLibrary;
+
+namespace LID_CLI {
+    class Program {
+        static void Main(string[] args) {
+            Framework 
+            Thread.Sleep(5000);
+        }
+    }
+}
